d88d35789f7e1e91b9be6ea2488283b0bd63a5c1
Nuhylmz <senturknuhyilmaz@gmail.com>
2022-03-30 23:08:09 +0300

    user route

--- client/app/admin/routes.js
+++ client/app/admin/routes.js
@@ -22,7 +22,12 @@ routesAdmin.route('/category', {
     this.render('adminLayoutDefault', { page: 'adminPageCategory' });
   }
 });
-
+routesAdmin.route('/user', {
+  name: 'admin.movie',
+  action: function (params, queryParams) {
+    this.render('adminLayoutDefault', { page: 'adminPageUser' });
+  }
+});
 routesAdmin.route('/movie', {
   name: 'admin.movie',
   action: function (params, queryParams) {
