0a5ca7a34776d39728a6567bce9e502eef61a6b9
Bilal <blalsnmez.8@gmail.com>
2022-03-30 12:10:47 +0300

    fix Str to html

--- server/methods/category/listByPoster.js
+++ server/methods/category/listByPoster.js
@@ -11,12 +11,11 @@ new ValidatedMethod({
 
     const result = Fetch(Category, {}, options, 'category');
     result.category = result.category.map(_category => {
-      //result.category[index].image_url = "https://via.placeholder.com/290x435.png?text=Category%20Empty";
       let movie = Movie.findOne({category_id: _category._id});
       if (movie) {
         _category.image_url = movie.cover_url;
       } else {
-        _category.image_url = "https://via.placeholder.com/290x435.png?text=Category%20Empty";
+        _category.image_url = "https://via.placeholder.com/446x664.png?text=Category%20Empty";
       }
       return _category;
     });
