f3d3a0f504e428b5f106d406602b8f9c61d8c498
Nuhylmz <senturknuhyilmaz@gmail.com>
2022-03-31 22:15:26 +0300

    dashboard stats

--- client/app/admin/pages/dashboard/index.js
+++ client/app/admin/pages/dashboard/index.js
@@ -1,15 +1,51 @@
 Template.adminPageDashboard.onCreated(function () {
   this.state = new ReactiveDict(null, {
-          
+    category:Number,
+    movie:Number,
+    comment:Number,
+    user:Number,
+    view:Number,
   });
 
 });
 
 Template.adminPageDashboard.onRendered(function () {
   const self = this;
-
   this.autorun(function () {
-
+    Meteor.call("category.list", {}, function (error, result) {
+      if (error) {
+        ErrorHandler.show(error.message);
+        return;
+      }
+      self.state.set("category", result.category.length);
+    });
+    Meteor.call("movie.list", {}, function (error, result) {
+      if (error) {
+        ErrorHandler.show(error.message);
+        return;
+      }
+      self.state.set("movie", result.movie.length);
+      let counter=0;
+      console.log(result.movie);
+      result.movie.forEach(function (data){
+        counter += data.views;
+      })
+      self.state.set("view", counter);
+    });
+    Meteor.call("comment.list", {}, function (error, result) {
+      if (error) {
+        ErrorHandler.show(error.message);
+        return;
+      }
+      self.state.set("comment", result.comments.length);
+    });
+    Meteor.call("user.list", {}, function (error, result) {
+      if (error) {
+        ErrorHandler.show(error.message);
+        return;
+      }
+      self.state.set("user", result.user.length);
+    });
   });
 });
 
